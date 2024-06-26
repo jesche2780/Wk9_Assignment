89f839c55714c3b55e4401b401136d27ec4a43fc
jesche2780 <papajames82@gmail.com>
2024-06-25 18:51:30 -0600

    Layout before coding

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+
+Project: WAR
+
+Suggested classes:
+Card, Deck, Player
+What properties and methods they would include?
+
+Notes:
+No user input needed the entire game should play out instantly without any user input inside of the browser's console.
+Nothing needs to happen that's special when there is a tie in a round.
+
+When completed, the game should do the following:
+ - Deal 26 Cards to each Player from a Deck of 52 cards.
+ - Iterate through the turns where each Player plays a Card.
+ - The Player who played the higher card is awarded a point.
+     - Ties result in zero points for both Players.
+ - After all cards have been played, display the score and declare the winner.
+
+--------------------- Code is below -----------------------*/
+
---
+++ tests.js
@@ -0,0 +1,6 @@
+const expect = chai.expect
+const assert = chai.assert
+
+describe('Javascript Unit Final:', () => {
+    
+})
