eeeb0795522fcf3b7e86c9d896c99b08dee55c93
jesche2780 <papajames82@gmail.com>
2024-06-27 08:20:48 -0600

    Added comments for code explanation

--- index.js
+++ index.js
@@ -31,14 +31,19 @@ When completed, the game should do the following:
 --------------------- Code is below -----------------------*/
 
 let cards = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
+// Declared cards number variable by creating an array with each card declared in a deck.
 
 let cardValues = {};
 for(let i = 0; i < cards.length; i++) {
     cardValues[cards[i]] = i+2;
 }
 
+// Each card number variable is given a value with a for loop that goes through the numbers until the end of the cards array is reached to give each card a hierarchical placement for later evaluation.
+
 const suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades'];
 let deck = [];
+
+// Declared each suit in a deck and opened an array called "deck" to be filled in with later.
 
 for(let suit of suits) {
     for(let card of cards){
@@ -46,15 +51,21 @@ for(let suit of suits) {
     }
 }
 
+// Used a "for let of" loop to fill in the above deck with a push action to build the "deck" array with the number and suit of the deck but also to set the "card" "value" to the value of the cardValue set above.
+
 for (let i = deck.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[j]] = [deck[j], deck[i]];
 }
 
+// Using the "Fisher Yates Shuffle" algorithm to randomize the accumulated deck.
+
 let Deck1 = deck.slice(0,25);
 let Deck2 = deck.slice(26,51);
 let Player1 = 0;
 let Player2 = 0;
+
+// "Slicing" the deck in half between two decks and starting the points for each player.
 
 console.log("Cards have been shuffled! Let's Play War!");
 
@@ -73,7 +84,12 @@ for (let i = 0; i < 25; i++) {
     }
 }
 
+// Running a for loop that will go through each array of both decks "i", adding by 1 each time, comparing each ranked card "value" against each other to find the winner and awarding that player with a point
+// each time that deck is higher. If both ranked cards' "value" are the same, each player gets a point. 
+
 console.log(`We have a winner! Final points: Player 1 - ${Player1} Player 2 - ${Player2}`)
+
+// Logging to the console the final points results
 
 if(Player1 > Player2) {
     console.log('Player 1 Wins!');
@@ -83,5 +99,5 @@ if(Player1 > Player2) {
     console.log('It\'s a Tie!');
 }
 
-
+// Creating an "else if" statement that will compare which player has the higher points and logs to the console which player wins.
 
